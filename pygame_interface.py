c39f44034f60717d65d36e1fef530b6a55d28249
Mohammed Ehsan <mohammedehsan363@gmail.com>
2025-04-08 00:50:17 +0400

    Dahsboard finished

--- pygame_interface.py
+++ pygame_interface.py
@@ -13,7 +13,7 @@ pygame.display.set_icon(logo)
 font_start = pygame.font.Font(None, 40)
 otn = font_start.render("Start", True, (255, 255, 255))
 
-font_entry = pygame.font.Font(None,36)
+font_entry = pygame.font.Font(None,60)
 entry_color_inactive = pygame.Color((139, 78, 57))
 entry_color_active = pygame.Color((103,37,14))
 color = entry_color_inactive
@@ -21,10 +21,13 @@ w_active = False
 b_active = False
 white_player = ''
 black_player = ''
-black_player_entry = pygame.Rect(100, 100, 140, 40)
-white_player_entry = pygame.Rect(100, 100, 140, 40)
+white_player_entry = pygame.Rect(90, 500, 140, 70)
+black_player_entry = pygame.Rect(1425, 500, 140, 70)
 
 start_button = pygame.Rect(850,750,220,60)
+playername_font = pygame.font.Font(None,50)
+playername_suface_txt = playername_font.render('Name',True,(110, 110, 111))
+covering_text = playername_font.render('Name',True,(220,220,220))
 
 
 def game_start(p1,p2):
@@ -38,15 +41,21 @@ while run:
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             if start_button.collidepoint(event.pos):
+                print('white: ',white_player)
+                print('black:',black_player)
                 game_start('Player 1','Player 2')
             if white_player_entry.collidepoint(event.pos):
-                w_active = not w_active
-            if black_player_entry.collidepoint(event.pos):
-                b_active = not b_active
+                w_active = True
+                b_active = False
+            elif black_player_entry.collidepoint(event.pos):
+                b_active = True
+                w_active = False
             else:
                 w_active = False
                 b_active = False
-            color = entry_color_active if w_active else entry_color_inactive
+
+        w_color = entry_color_active if w_active else entry_color_inactive
+        b_color = entry_color_active if b_active else entry_color_inactive
         if event.type == pygame.KEYDOWN:
             if w_active:
                 if event.key == pygame.K_BACKSPACE:
@@ -63,18 +72,27 @@ while run:
     screen.blit(dashboard_img,(0,0))
     pygame.draw.rect(screen,'GREEN',start_button,border_radius=15)
     screen.blit(otn, (start_button.x + 75, start_button.y + 15))
-    w_txt_surface = font_entry.render(white_player, True, color)
-    b_txt_surface = font_entry.render(black_player, True, color)
+    w_txt_surface = font_entry.render(white_player, True, w_color)
+    b_txt_surface = font_entry.render(black_player, True, b_color)
 
-    width = max(200, w_txt_surface.get_width()+10)
-    width = max(200, b_txt_surface.get_width()+10)
-    white_player_entry.w = width
-    black_player_entry.w = width
+    if len(white_player) == 0:
+        screen.blit(playername_suface_txt, (110, 520))
+    if len(black_player) == 0:
+        screen.blit(playername_suface_txt, (1450, 520))
+    if len(black_player) != 0:
+        screen.blit(covering_text,(1450, 520))
+    if len(white_player) != 0:
+        screen.blit(covering_text,(100, 520))
+        
+    w_width = max(400, w_txt_surface.get_width() + 10)
+    b_width = max(410, b_txt_surface.get_width() + 10)
+    white_player_entry.w = w_width
+    black_player_entry.w = b_width
 
-    screen.blit(w_txt_surface, (white_player_entry.x+50, white_player_entry.y+5))
-    screen.blit(b_txt_surface, (black_player_entry.x+5, black_player_entry.y+50))
-    pygame.draw.rect(screen, color, black_player_entry, 2)
-    pygame.draw.rect(screen, color, white_player_entry, 2)
+    screen.blit(w_txt_surface, (white_player_entry.x+5, white_player_entry.y+5))
+    screen.blit(b_txt_surface, (black_player_entry.x+5, black_player_entry.y+5))
+    pygame.draw.rect(screen, b_color, black_player_entry, 5, border_radius=10)
+    pygame.draw.rect(screen, w_color, white_player_entry, 5, border_radius=10)
 
     pygame.display.update()
     clock.tick(60)
--- test.py
+++ test.py
@@ -1,14 +1,54 @@
-from os import WCONTINUED
-
 import pygame
 pygame.init()
-screen = pygame.display.set_mode((1300,1000))
-run = 1
-while run==1:
-    for events in pygame.event.get():
-        if events.type == pygame.QUIT:
-            run = 0
+
+# Window setup
+screen = pygame.display.set_mode((640, 480))
+pygame.display.set_caption("Text Input Box")
+
+# Fonts and colors
+font = pygame.font.Font(None, 36)
+input_box = pygame.Rect(100, 100, 140, 40)
+color_inactive = pygame.Color((139, 78, 57))
+color_active = pygame.Color((103,37,14))
+color = color_inactive
+active = False
+text = ''
+
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # If the user clicked on the input_box rect.
+            if input_box.collidepoint(event.pos):
+                active = not active
+            else:
+                active = False
+            color = color_active if active else color_inactive
+
+        if event.type == pygame.KEYDOWN:
+            if active:
+                if event.key == pygame.K_RETURN:
+                    print("Input:", text)
+                    text = ''
+                elif event.key == pygame.K_BACKSPACE:
+                    text = text[:-1]
+                else:
+                    text += event.unicode
+
+    screen.fill((30, 30, 30))
+    txt_surface = font.render(text, True, color)
+    width = max(200, txt_surface.get_width()+10)
+    input_box.w = width
+    screen.blit(txt_surface, (input_box.x+5, input_box.y+5))
+    pygame.draw.rect(screen, color, input_box, 2)
+
+    pygame.display.flip()
+    clock.tick(30)
+
 pygame.quit()
 
-def aaaa():
-    WCONTINUED
